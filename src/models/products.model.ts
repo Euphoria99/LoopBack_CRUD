29072ab34137b365460c467f3352ff44ba05e567
PavanBhat <pavan.bhat@thewebpeople.in>
2022-10-18 16:52:36 +0530

    Created 'Entity' model

---
+++ src/models/products.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Products extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  title?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  quantity: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+
+  constructor(data?: Partial<Products>) {
+    super(data);
+  }
+}
+
+export interface ProductsRelations {
+  // describe navigational properties here
+}
+
+export type ProductsWithRelations = Products & ProductsRelations;
